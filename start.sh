391cd830303edfe221a7a2c4ff01de58f59e81c9
cole <cole@mbuntu.com>
2015-08-01 16:30:19 +0800

    + simple photo wall

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$NODE_PATH" = "" ] && [ ! -d "node_modules" ]; then
+    npm install
+    # npm install --registry=https://registry.npm.taobao.org
+fi
+
+gulp || (npm install && gulp)
